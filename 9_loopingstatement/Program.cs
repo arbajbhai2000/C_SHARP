a39d834101441034b31421160179c6140bc4dbe8
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-11 11:09:11 +0530

    Looping Statement

---
+++ 9_loopingstatement/Program.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_loopingstatement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            #region while loop
+        {
+
+            //int start = 1;
+            //while (start <= 5) 
+            //{
+            //    Console.WriteLine("arbaj".ToUpper());
+            //    start++;
+            //}
+            #endregion while loop
+
+            #region do while loop
+            //int start = 1;
+            //do
+            //{
+            //    Console.WriteLine(start);
+            //    start++;
+            //}
+            //while(start <=5);
+            #endregion do while loop
+
+            #region for loop
+            //for (int start=1;start<=11;start++)
+            //{
+            //    Console.WriteLine(start);
+            //}
+
+
+            //for (int start=1;start<=5;start++)
+            //{ 
+            //    if (start == 4 )
+            //    {
+            //        //break;
+            //        continue; 
+            //    }
+            //    Console.WriteLine(start);
+            //}
+
+            //for (int start = 1; start <= 5; start++)
+            //{
+            //    Console.WriteLine(start);
+            //    if (start == 3)
+            //    {
+            //        break;
+            //        //continue;
+            //    }
+
+            //}
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    if (i == 2 && i==4)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 10; i >= 1; --i)
+            //{
+            //    if (i == 12 && i == 41)
+            //    {
+            //        continue;
+            //    }Console.WriteLine(i);
+            //}
+            #endregion for loop
+
+            #region * pattern 
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= 5; j++)
+            //    {
+            //        Console.Write($"*\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 5; i > 0; i--) 
+            //{
+            //    for(int j=1; j <= i; j++)                
+            //    {
+            //        Console.Write($"*\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //for (int i = 5; i > 0; i--)
+            //{
+            //    for (int j = 1; j <=i; j++) 
+            //    {
+            //        Console.Write($"{i}{j}\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //for (int i = 5; i >= 1; --i)
+            //{
+            //    for (int j = 1; j <= i; ++j)
+            //    {
+            //        Console.Write($"*\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //    for (int arb = 1; arb <= 6; arb++) 
+            //{
+            //    for(int b=arb; b <= arb; b++) 
+            //    {
+            //        Console.Write($"arbaj\t");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            //for (int row = 1; row <= 8; ++row)
+            //{
+            //    for (int col = 1; col <= row; ++col)
+            //    {
+            //        Console.Write($"*\t");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+            //for (int i = 1; i <= 5; ++i)
+            //{
+            //    for (int j =1;j <=i; j++)
+            //    {
+            //        Console.Write("novfil\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int i=1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("**\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion * pattern 
+
+            #region Equilateral triangle
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for(int j = 1; j <= 10 - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion Equilateral triangle
+
+            #region pattern 2
+            //for (int i=1; i <5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //        Console.Write(" * ");
+            //    Console.WriteLine();
+
+            //}
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //        Console.Write(" * ");
+            //    Console.WriteLine();
+            //}
+            #endregion pattern 2
+
+            #region pattern 3
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j = 1; j <= i; j++)
+
+            //        Console.Write(" * ");
+            //    Console.WriteLine();
+
+            //}
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //        Console.Write(" * ");
+            //    Console.WriteLine();
+            //}
+            #endregion pattern 3
+
+            #region square pattern
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= 5; j++)
+            //        Console.Write(" * ");
+            //    Console.WriteLine();
+            //}
+            #endregion square pattern
+
+
+
+
+
+            Console.ReadLine();
+        
+        }
+    }
+}
+    
+        
