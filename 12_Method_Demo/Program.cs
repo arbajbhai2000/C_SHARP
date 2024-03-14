8f91458e06657aa9220d68fe8f05e73ddcc38831
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-14 16:58:34 +0530

    Method

---
+++ 12_Method_Demo/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _12_Method_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintA();
+
+            PrintB("ARBAJ");
+            string name = "NOVFIL";
+            PrintB(name);
+
+            PrintC("ARBAJ", "MUJAWAR");
+            string firstname = "NOVFIL", lastname = "MUJAWAR";
+            PrintC(firstname, lastname);
+
+            string output = PrintD("SWAPNIL", "KOLHE");
+            Console.WriteLine($"PrintD fullname :{output}");
+
+            PrintE(true);
+            PrintE(false);
+
+            PrintF(null);
+            int[] number = new int[] { 10, 30, 4, 50 };
+            PrintF(number);
+
+
+
+            Console.ReadLine();
+        }
+
+        //without return without parameter 
+        static void PrintA()
+        {
+            Console.WriteLine($"method called PrintA");
+        }
+        //with paramter 
+        static void PrintB(string name)
+        {
+            Console.WriteLine($"PrintB : Hello {name}");
+        }
+        //with multiple parameter
+        static void PrintC(string fn, string ln)
+        {
+            Console.WriteLine($"PrintC fullname :{fn} {ln}");
+        }
+
+        //PARAMETER
+        static string PrintD(string fn, string ln)
+        {
+            return (fn + " " + ln);
+        }
+
+        //return statement 
+        static void PrintE(bool isconfirmed)
+        {
+            if (!isconfirmed)
+            {
+                return;
+            }
+            Console.WriteLine("session allow");
+        }
+
+        //sum of array element
+
+        static void PrintF(int[] number)
+        {
+            if(number !=null &&  number.Length > 0)
+            {
+                int sum = 0;
+                for (int i = 0; i < number.Length; i++) 
+                {
+
+                    sum += number[i];
+                }
+                Console.WriteLine($"enter number sum : {sum}");
+            }
+            else
+            {
+                Console.WriteLine("no number to add");
+            }
+        }
+        
+
+
+    }
+}
---
+++ 12_Method_Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("12_Method_Demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("12_Method_Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f299575c-4d15-476f-8b0f-3b5332db7175")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
