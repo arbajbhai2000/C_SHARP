791f62065216c3ec108f702887d65d05a33a3566
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-16 13:43:59 +0530

    Optional Method Parameter

---
+++ 14_optionalMethodParameter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_optionalMethodParameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           add(10, 20);
+
+            //add(20);
+
+            //add();
+
+            add(b: 20);//named paramter
+
+
+            Console.ReadLine(); 
+        }
+
+        //static void add(int a, int b)
+        //{
+        //    Console.WriteLine($"addition {a} + {b}= {a + b}");
+        //}
+
+        //static void add(int a = 20,int b =40)
+        //{
+        //    Console.WriteLine($"addition {a} + {b} = {a + b}");
+        //}
+
+        //static void add(int a , [Optional] int b) //for default value use optional 
+        //{
+        // Console.WriteLine($"addition {a} + {b} = {a + b}");  //default value it takes 0
+        //}
+
+        static void add([Optional]int a,  int b)
+        {
+            Console.WriteLine($"addition {a} + {b} = {a + b}");  
+        }
+
+    }
+}
