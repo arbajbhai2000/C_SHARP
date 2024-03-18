fc36b6543aafb67a59d5c49fa029dd91d09eb3e7
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-18 10:43:22 +0530

    Boxing And Unboxing

---
+++ 16_BoxingAndUnboxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_BoxingAndUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine($"i :{i}");
+            object o = i;//int ->object  value type to reference type =boxing
+            Console.WriteLine($" o : {o}");
+
+            int j = (int)o; //object -> int reference type -> value type = unboxing 
+            Console.WriteLine($" j : {i}");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
