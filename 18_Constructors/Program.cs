bc0e8b751ecd889de57861aca9111b2ecc4ae9a5
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-20 12:48:34 +0530

    Constructor

---
+++ 18_Constructors/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            student s1 = new student();  //object creation
+            s1.details();
+
+            //student s2 = new student();
+            //s2.rollnumber = 1;          //dot operator
+            //s2.firstname = "arbaj";
+            //s2.lastname = "mujawar";
+            //s2.details();
+
+            ////object initiallizer syntax
+            //student s3 = new student()
+            //{
+            //    rollnumber = 2,
+            //    firstname = "novfil",
+            //    lastname = "mujawar"
+            //};
+            //s3.details();
+
+            //student s4 = new student();
+            //s4.initialize(3,"suraj","pathan");
+            //s4.details();
+
+            //student s5 = new student();
+            //s5.details();
+
+            //student s6 = new student(6,"jishan","kazi");
+            //s6.details();
+
+            //student s7 = new student();
+            //student s8 = new student(8,"a" ,"b");
+
+            //student s9 = new student(9,"sohel","mukeri");
+            //s9.details();
+
+            //student s10 = s9;
+            //s10.details();
+
+            //student s11 = new student();
+            //s11.rollnumber = s9.rollnumber;
+            //s11.firstname= s9.firstname;
+            //s11.lastname= s9.lastname;
+            //s11.details();
+
+            //student s12 = new student(s11);
+            //s12.details();
+
+            //student s13 = new student();
+            //s13.details();
+            //student s14 = new student();
+            //s14.details();
+            //student s15 = new student();
+            //s15.details();
+
+            //student s16 = new student();
+
+            //student s17 = new student();
+
+            Console.WriteLine($"course name :{student.coursename}");
+
+            student s18 = new student();
+            Console.WriteLine($"coursename :{student.coursename}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 18_Constructors/student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Constructors
+{
+    public class student
+    {
+
+        public int rollnumber;  //field
+        public string firstname;
+        public string lastname;
+        public static string coursename;
+        //default constructor or paramter less constructor
+        public student()        //constructor syntax it not used return type or void
+        {
+            Console.WriteLine($"student() constructor called");
+            rollnumber = 20 ;
+            firstname = "jishan";
+            lastname = "kazi";
+           // coursename = "dot net";
+        }
+
+        //parameterized constructor
+        public student(int rn, string fn, string ln)
+        {
+            Console.WriteLine("student (int rn ,string fn,string ln ) constructor called parameter");
+            rollnumber = rn;
+            firstname = fn;
+            lastname = ln;
+        }
+
+        //copy constructor
+        public student (student s)
+        {
+            this.rollnumber = s.rollnumber;
+            this.firstname = s.firstname;
+            this.lastname = s.lastname;
+        }
+
+        static student()
+        {
+            Console.WriteLine($"static student() constructor called");
+            coursename = "dot net";
+        }
+         
+        public void details()   //method
+        {
+            Console.WriteLine($"Rollnumber : {rollnumber} Fullname : {firstname} {lastname} coursename :{coursename}");
+        }
+
+        //public void initialize( int rn ,string fn ,string ln )
+        //{
+        //    rollnumber = rn;
+        //    firstname = fn;
+        //    lastname = ln;
+        //} 
+    }
+}
