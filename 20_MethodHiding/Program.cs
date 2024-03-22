655721956f45a5645f29e1a78b7d18e67b09e478
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-22 19:44:33 +0530

    Method Hiding

---
+++ 20_MethodHiding/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_MethodHiding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            customer c1 = new customer();
+            c1.print();
+            c1.printA();
+            //c1.printB(); //not possible because it is a child class
+
+            silvercustomer sc = new silvercustomer();
+            sc.print();
+            sc.printA();
+            sc.printB();
+
+            customer c2 = new silvercustomer();
+            c2.print();
+            c2.printA();
+            //c2.printB();
+            ((silvercustomer)c2).printB(); //use the type cast operator to get the print B output 
+
+            //silvercustomer sc2= new customer();
+            //sc2.print();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 20_MethodHiding/customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_MethodHiding
+{
+    public class customer
+    {
+        public void print()
+        {
+            Console.WriteLine($"customer print() method called");
+        }
+
+        public void printA()
+        {
+            Console.WriteLine($"customer printA() method called");
+        }
+    }
+
+    public class silvercustomer :customer 
+    {
+        new public void print()   //use new for new method
+        {
+            Console.WriteLine($"silvercustomer print() method called");
+        }
+
+        public void printB()
+        {
+            Console.WriteLine($"silvercustomer printB() method called");
+        }
+    }
+}
