b668ab9665ac0085a06e202dbb61e0e4705e2f94
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-24 20:05:11 +0530

    Methodoverriding que

---
+++ 22_Methodoverriding_questions/A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_Methodoverriding_questions
+{
+    public class A
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("a");
+        }
+    }
+    public class B:A
+    {
+        public override void print()
+        {
+            Console.WriteLine("B");
+        } 
+    }
+    public class C:B
+    {
+        public override void print()
+        {
+            Console.WriteLine("C");
+        }
+
+
+    }
+}
---
+++ 22_Methodoverriding_questions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_Methodoverriding_questions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 =new A(); a1.print();
+            A a2 =new B(); a2.print();
+            A a3 =new C(); a3.print();
+            B b1=new C(); b1.print();
+
+
+            Console.ReadLine();
+        }
+    }
+}
