faf1f02e051b0479f1073cd9290629fceb59150f
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-25 12:15:09 +0530

    Multipleclass Inheritance

---
+++ 23_MultipleClassInheritenceproblem/A.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_MultipleClassInheritenceproblem
+{
+    public class A
+    {
+         public virtual void print()
+        {
+            Console.WriteLine("A");
+        }
+    }
+    public class B:A
+    {
+        public override void print()
+        {
+            Console.WriteLine("B");
+        }
+    }
+    public class C:B
+    {
+
+    }
+}
---
+++ 23_MultipleClassInheritenceproblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_MultipleClassInheritenceproblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a1 = new A();
+            a1.print();
+
+            B b1 = new B();
+            b1.print();
+
+            C c1 = new C();
+            c1.print();
+
+            Console.ReadLine();
+        }
+    }
+}
