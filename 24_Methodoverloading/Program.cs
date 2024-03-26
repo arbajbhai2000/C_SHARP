08a614b642756862f41ba413e44bb839d2ff12e9
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-26 13:14:26 +0530

    Methodoverloading

---
+++ 24_Methodoverloading/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Methodoverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculator c1 = new calculator();
+            c1.print();
+
+            c1.print(10);
+
+            int i = 10;
+
+            c1.print(ref i);
+
+            c1.print(20,30);
+
+            c1.print("arbaj");
+
+            c1.print(1, "novfil");
+
+            c1.print("novfil", 1);
+
+            short s = 10;
+            int i1 = 10;
+            c1.print(10);
+            c1.print(s);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 24_Methodoverloading/calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_Methodoverloading
+{
+    public class calculator
+    {
+        public void print()
+        {
+            Console.WriteLine("print() called");
+        }
+
+        public void print(int num)
+        {
+            Console.WriteLine("print() int num called");
+        }
+
+        public void print(short num)
+        {
+            Console.WriteLine("print(short num) called");
+        }
+
+        public void print(ref int num)
+        {
+            Console.WriteLine("print(ref int num) called");
+        }
+
+        public void print(int num1 ,int num2)
+        {
+            Console.WriteLine("print() int num1,int num2");
+        }
+
+        public void print(string name)
+        {
+            Console.WriteLine("print() string name");
+        }
+
+        public void print(int num ,string name)
+        {
+            Console.WriteLine("print(int num ,string name) ");
+        }
+
+        public void print(string name,int num)
+        {
+            Console.WriteLine("print(string name,int num) ");
+        }
+
+        public void print(int[] num)
+        {
+            Console.WriteLine("print( int[] num) called");
+        }
+         
+        //public void print(params int[] num)  //not possible to overloading
+        //{
+        //    Console.WriteLine("print( int[] num) called");
+        //}
+    }
+}
