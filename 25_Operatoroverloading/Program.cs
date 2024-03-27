5a1887926a7d33781ab27f74be5706a7826f2f97
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-27 14:34:19 +0530

    operator overloading

---
+++ 25_Operatoroverloading/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_Operatoroverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region int type == operator
+            //int i = 10;
+            //int j = 10;
+            //if (i == j)
+            //{
+            //    Console.WriteLine("i & j are equal");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("i & j are not equal");
+            //}
+            #endregion int type == operator
+
+            #region string type == operator
+            //string s1 = "arbaj";
+            //string s2 = s1;
+            //if(s1 == s2)
+            //{
+            //    Console.WriteLine("s1 & s2 are equal");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("s1 & s2 are not equal");
+            //}
+            #endregion string type == operator
+
+            #region student
+            //student s1 = new student(1, "arbaj", "mujawar");
+
+            ////student s2 = s1;
+            //student s2 = new student(1, "arbaj", "mujawar");
+            //if (s1== s2)
+            //{
+            //    Console.WriteLine("s1 & s2 are equal");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("s1 & s2 are not equal");
+            //}
+            #endregion student
+
+            student s1 = new student(1,"arbaj","mujawar");
+
+            student s2 = new student(3, "suraj", "pathan");
+
+            student s3 = s1 + s2;
+
+            Console.WriteLine($"{s3.rollnumber}: {s3.firstname}: {s3.lastname}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 25_Operatoroverloading/student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_Operatoroverloading
+{
+    public class student
+    {
+        public int rollnumber;
+        public string firstname;
+        public string lastname;
+
+        public student(int rn, string fn, string ln)
+        {
+            rollnumber =  rn ;
+            firstname = fn ;
+            lastname = ln ;
+        }
+        
+        public static bool operator==( student s1,student s2)
+        {
+            return s1.rollnumber == s2.rollnumber && s1.firstname == s2.firstname &&
+                s1.lastname == s2.lastname;
+
+        }
+
+        public static bool operator !=(student s1, student s2)
+        {
+            return s1.rollnumber == s2.rollnumber && s1.firstname == s2.firstname &&
+                s1.lastname == s2.lastname;
+
+        }
+        public static student operator +(student s1,student s2)
+        {
+            int rollnumber = s1.rollnumber + s2.rollnumber;
+            string firstname = s1.firstname + "," + s2.firstname;
+            string lastname = s1.lastname + "," + s2.lastname;
+            return new student(rollnumber, firstname, lastname);
+        }
+
+
+    }
+}
