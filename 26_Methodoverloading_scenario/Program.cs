2534cc80e9809d51864fb144e9242a013afa6a0b
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-03-28 14:22:44 +0530

    Method overloading scenario

---
+++ 26_Methodoverloading_scenario/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_Methodoverloading_scenario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a=new A();
+            a.printA();
+
+            B b=new B();
+            b.printA(10);   
+            b.printA();
+
+            Console.ReadLine();
+        }
+    }
+    class A
+    {
+        public void printA()
+        {
+            Console.WriteLine("A:printA()");
+        }
+    }
+    class B:A
+    {
+        public void printA(int a)
+        {
+            Console.WriteLine("B: printB(int a)");
+        }
+    }
+}
