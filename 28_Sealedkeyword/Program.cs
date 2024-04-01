1815a64fa4179bf457d399e7624b7db127bf48e6
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-04-01 11:03:00 +0530

    sealedkeyword

---
+++ 28_Sealedkeyword/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Sealedkeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            customer c1 = new customer();
+            c1.print();
+
+            customer c2 = new silvercustomer();
+            c2.print();
+
+            customer c3 = new advancedsilvercustomer();
+            c3.print();
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 28_Sealedkeyword/customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Sealedkeyword
+{
+    //public sealed class customer
+    public class customer
+    {
+        public virtual void print()
+        {
+            Console.WriteLine("customer print");
+
+        }
+    }
+    public class silvercustomer : customer
+    {
+        public sealed override void print()
+        {
+            Console.WriteLine("silver customer");
+        }
+    }
+    public class advancedsilvercustomer : silvercustomer
+    {
+        public override void print()    //due to sealed advance class cannot override 
+        {
+            Console.WriteLine("advanced silver customer");
+        }
+    }
+    // public class Student { } // normal class
+    // public static class Student { } // static class
+    // public abstract class Student { } // abstract class
+    // public static abstract class Student { } // static & abstract at same time not possible
+    // public sealed class Student { } // sealed class
+    // public static sealed class Student { } // static & sealed at same time not possible
+}
