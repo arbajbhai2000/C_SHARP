a9a2582fb1a7813349d10e07eb1d0a3a5d2e45fb
Arbaj Mujawar <115889948+arbajbhai2000@users.noreply.github.com>
2024-04-10 16:28:56 +0530

    Namespace

---
+++ 30_NamespaceDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;   //to import namespace
+
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("hello");
+
+        silvercustomer sc=new silvercustomer();
+        sc.details();
+
+        savingscustomer sc1=new savingscustomer();
+        sc1.details();
+        Console.ReadLine();
+    }
+}
+class silvercustomer
+{
+    public void details()
+    {
+        Console.WriteLine("silver customer");
+    }
+}
+class savingscustomer
+{
+    public void details()
+    {
+        Console.WriteLine("savings customer");
+    }
+}
